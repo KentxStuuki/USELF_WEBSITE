978e753ae231e1852c7ff7d63e7d8fe7c76cdd3a
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-03-17 18:04:58 +0800

    todo list added

--- utsfinals/src/app/app.component.ts
+++ utsfinals/src/app/app.component.ts
@@ -1,23 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-import { ToDoComponent } from './to-do/to-do.component';
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ToDoComponent],
-=======
 import { HomeIntroComponent } from './components/home-intro/home-intro.component';
 import { ExplanationComponent } from './components/explanation/explanation.component';
 import { WillHappenComponent } from './components/will-happen/will-happen.component';
 import { WhatCanDoComponent } from './components/what-can-do/what-can-do.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ToDoComponent } from './to-do/to-do.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeIntroComponent, ExplanationComponent, WillHappenComponent, WhatCanDoComponent, FooterComponent],
->>>>>>> ad26acf10366ba9d50df123b686717e4bb61f1fb
+  imports: [RouterOutlet, HomeIntroComponent, ExplanationComponent, WillHappenComponent, WhatCanDoComponent, FooterComponent, ToDoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
