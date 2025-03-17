2cb2c0e87944eddf0251056f2b316e4632d61307
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-03-17 18:50:42 +0800

    climate psych section added

--- utsfinals/src/app/components/to-do/to-do.component.ts
+++ utsfinals/src/app/components/to-do/to-do.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { InformationService } from '../information.service';
+import { InformationService } from '../../information.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
