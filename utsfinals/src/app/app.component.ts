ad26acf10366ba9d50df123b686717e4bb61f1fb
KentXStuuki <mequiambao@outlook.com>
2025-03-17 17:30:00 +0800

    Styles has been added

--- utsfinals/src/app/app.component.ts
+++ utsfinals/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeIntroComponent } from './components/home-intro/home-intro.component';
+import { ExplanationComponent } from './components/explanation/explanation.component';
+import { WillHappenComponent } from './components/will-happen/will-happen.component';
+import { WhatCanDoComponent } from './components/what-can-do/what-can-do.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeIntroComponent, ExplanationComponent, WillHappenComponent, WhatCanDoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'utsfinals';
+
 }
---
+++ utsfinals/src/app/components/will-happen/will-happen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-will-happen',
+  standalone: true,
+  imports: [],
+  templateUrl: './will-happen.component.html',
+  styleUrl: './will-happen.component.css'
+})
+export class WillHappenComponent {
+
+}
