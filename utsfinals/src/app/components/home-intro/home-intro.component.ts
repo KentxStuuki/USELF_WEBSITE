0872be01407a5b205b610d9f30a9d9e7bb7b7740
KentXStuuki <mequiambao@outlook.com>
2025-03-17 18:52:11 +0800

    First half almost done

--- utsfinals/src/app/components/home-intro/home-intro.component.ts
+++ utsfinals/src/app/components/home-intro/home-intro.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-intro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-intro.component.html',
   styleUrl: './home-intro.component.css'
 })
 export class HomeIntroComponent {
+  // Home Header Introduction
+  home_header = [
+    "MALAPIT NA ANG HULI",
+    "UNDERSTANDING CLIMATE PSYCHOLOGY THROUGH THE ",
+    "2030 CLIMATE CHANGE DEADLINE"
+  ] 
 
+  year = "irreversible damage ------------ 2030"
+
+  years = [
+    "2029",
+    "2028",
+    "2027",
+    "2026",
+    "current ------------ 2025"
+  ]
 }
